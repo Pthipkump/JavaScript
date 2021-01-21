03c0b947c9b08e2a6a83d5120fab4dfca9a17dc5
Punnawat Thipkump <punnawat@siamdapp.com>
2021-01-21 10:17:34 +0700

    add project function

---
+++ Project8- function/app.js
@@ -0,0 +1,7 @@
+function toCelsius()
+{
+    let fahrenheit = prompt("Fahrenheit:")
+    let value = (fahrenheit - 32) * 5 / 9
+    document.getElementById("answer_section").innerHTML = value.toFixed(2) + "℃";
+}
+toCelsius()
